1a1c753cc2c2beb4e0bf8bd40f2eb67fd460607b
cnDaniel <cndaniel@me.com>
2017-08-04 18:52:41 +0200

    If statment -lt -gt

--- print-services.sh
+++ print-services.sh
@@ -8,6 +8,9 @@ for file in services/*.service; do
   if grep -q port "$file" && grep -q name "$file"; then
     name=$(grep name "$file" | sed 's/name //')
     port=$(grep port "$file" | sed 's/port //')
+    if [ "$port" -lt 5000 ]; then
+      echo "Port is under 5000."
+    fi
     echo "In $file, we're running  $name on $port"
   else
     echo "$file does not have a name or a port."
