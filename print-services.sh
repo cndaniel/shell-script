ad9277b965a525b60a72482edfdca88b795b5b59
cnDaniel <cndaniel@me.com>
2017-08-04 18:38:08 +0200

    Show name and port

--- print-services.sh
+++ print-services.sh
@@ -7,6 +7,7 @@ set -o pipefail
 for file in services/*.service; do
   if grep -q port "$file" && grep -q name "$file"; then
     name=$(grep name "$file" | sed 's/name //')
-    echo "$file $name" 
+    port=$(grep port "$file" | sed 's/port //')
+    echo "$file $name $port"
   fi
 done
