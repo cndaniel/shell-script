98dca31db1d42015e3d63f69b79b7bf6969fa643
cnDaniel <cndaniel@me.com>
2017-08-04 18:34:02 +0200

    Parenthesis

--- print-services.sh
+++ print-services.sh
@@ -5,7 +5,8 @@ set -e
 set -o pipefail
 
 for file in services/*.service; do
-  if grep -q port "$file"; then
-    echo "$file" 
+  if grep -q port "$file" && grep -q name "$file"; then
+    name=$(grep name "$file" | sed 's/name //')
+    echo "$file $name" 
   fi
 done
